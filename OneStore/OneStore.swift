a498ea1359d24a86bdc81735393334671c4099f1
muukii <m@muukii.me>
2016-12-23 22:07:55 +0900

    Add synchronouslyOnMainThread in OneStore

--- OneStore/OneStore.swift
+++ OneStore/OneStore.swift
@@ -27,6 +27,9 @@ open class OneStore<T: OneStoreValueProtocol> {
   open let storeKey: String
   open let stack: Stack
 
+  /// Run get value, set value on "MainThread" synchronously.
+  open var synchronouslyOnMainThread: Bool = false
+
   public init(_ key: String, stack: Stack = Stack.defaultStack) {
 
     self.storeKey = key
@@ -41,10 +44,22 @@ open class OneStore<T: OneStoreValueProtocol> {
 
   open var value: T? {
     get {
-      return T.getOneStoreValue(stack.userDefaults, key: actualStoreKey)
+      if synchronouslyOnMainThread {
+        return DispatchQueue.main.sync {
+          return T.getOneStoreValue(stack.userDefaults, key: actualStoreKey)
+        }
+      } else {
+        return T.getOneStoreValue(stack.userDefaults, key: actualStoreKey)
+      }
     }
     set {
-      newValue?.setOneStoreValue(stack.userDefaults, key: actualStoreKey)
+      if synchronouslyOnMainThread {
+        DispatchQueue.main.sync {
+          newValue?.setOneStoreValue(stack.userDefaults, key: actualStoreKey)
+        }
+      } else {
+        newValue?.setOneStoreValue(stack.userDefaults, key: actualStoreKey)
+      }
     }
   }
 
